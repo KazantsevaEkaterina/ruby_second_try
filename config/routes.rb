d33fdd6d90ddcedd1f295b9dfdb1a3e998e515b6
Ekaterina12345 <iiiii21111998@gmail.com>
2021-05-14 18:23:13 +0300

    corrected image path, route to main

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,6 @@ Rails.application.routes.draw do
   match 'signin',   to: 'sessions#new',         via: 'get'
   match 'signout',  to: 'sessions#destroy',     via: 'delete'
 
-  root 'work#index'
+  root 'main#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
